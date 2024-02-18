7bf2b54e6f648dc2833e639d204f3e99fe8bda46
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-19 04:58:55 +0800

    Progress: Build Customer Controller

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index() : View {
+        return view('customer.index', [
+            'title' => 'Customer Page',
+        ]);
+    }
+}
