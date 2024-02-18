5698d0e31d9cd201d774a03d15497ccf5d4723fd
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-19 05:04:01 +0800

    Progress: Build Component Topbar

---
+++ resources/views/components/topbar.blade.php
@@ -0,0 +1,6 @@
+<div class="topbar d-flex align-items-center justify-content-between me-5">
+    <p class="topbar-title">Dashboard Page</p>
+    <div class="topbar-profile">
+        <img src="{{ asset('assets/images/profile/profile-not-found.jpg') }}" alt="Profile Image" class="rounded-full" width="46" height="46" style="object-fit: cover;">
+    </div>
+</div>
