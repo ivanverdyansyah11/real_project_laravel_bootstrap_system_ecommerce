aeabc95dca00331549fdac6afcccf1c1a2073976
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-19 04:59:11 +0800

    Progress: Build Product Controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() : View {
+        return view('product.index', [
+            'title' => 'Product Page',
+        ]);
+    }
+}
