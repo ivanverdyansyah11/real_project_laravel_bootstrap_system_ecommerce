f2c86e1d7b6c1704a9dcc6b4f8b1ae52576a1b6f
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:19:26 +0800

    Progress: Finish Product Migration

--- database/migrations/2024_02_16_082502_create_products_table.php
+++ database/migrations/2024_02_16_082502_create_products_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('categories_id');
+            $table->string('image');
+            $table->string('name', 255);
+            $table->text('description');
+            $table->enum('unit', ['box', 'pcs']);
+            $table->integer('purchase_price');
+            $table->integer('selling_price');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
