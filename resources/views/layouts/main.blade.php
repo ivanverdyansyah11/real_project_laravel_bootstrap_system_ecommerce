713b16909cc235827464b5bf4276b50fc7454277
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:24:08 +0800

    Progress: Finish layout Main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }} | E-StoreMate Ecommerce POS</title>
+    <title>{{ $title }} | Adigoeroe Ecommerce</title>
     
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.0/css/dataTables.dataTables.css" />
+    <link rel="stylesheet" href="{{ asset('assets/css/dataTables.bootstrap5.css') }}">
 </head>
 <body>
     @if (Route::is('login') || Route::is('register'))
@@ -20,10 +22,10 @@
     @else
         <div class="container-fluid">
             <div class="row">
-                <div class="col-2">
+                <div class="col-lg-3 col-xl-2">
                     @include('components.sidebar')
                 </div>
-                <div class="col">
+                <div class="col-12 col-lg-9 col-xl-10 pb-5">
                     @include('components.topbar')
                     @yield('content-dashboard')
                 </div>
@@ -33,6 +35,7 @@
 
     <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/js/script.js') }}"></script>
+    <script src="https://cdn.datatables.net/2.0.0/js/dataTables.js"></script>
     @stack('js')
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 </body>
