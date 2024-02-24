3796ef3537280df7c38d09d3eaf72fe8767c2132
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:18:59 +0800

    Progress: Finish User MIgration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->nullable();
+            $table->enum('role', ['super_admin', 'admin', 'reseller', 'customer']);
+            $table->integer('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
