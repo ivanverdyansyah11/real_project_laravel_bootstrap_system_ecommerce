ec16d86fe9348fa69aec840bfc8eba21c08d5a48
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:17:08 +0800

    Progress: Finish Reseller Model

--- app/Models/Reseller.php
+++ app/Models/Reseller.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reseller extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+
+    public function reward_transaction() {
+        return $this->hasMany(TransactionReward::class, 'id');
+    }
+
+    public function transaction() {
+        return $this->hasMany(Transaction::class, 'id');
+    }
 }
