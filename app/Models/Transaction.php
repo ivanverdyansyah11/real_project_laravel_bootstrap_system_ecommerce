48b63a9dfb56e9ca35d14e775be8306cea54def1
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:17:21 +0800

    Progress: Finish Transaction Model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function product() {
+        return $this->belongsTo(Product::class, 'products_id');
+    }
+
+    public function customer() {
+        return $this->belongsTo(Customer::class, 'customers_id');
+    }
+
+    public function reseller() {
+        return $this->belongsTo(Reseller::class, 'resellers_id');
+    }
 }
