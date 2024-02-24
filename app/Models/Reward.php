2fca3bc9043f49817267d7b2677e4f2bba92d5ee
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:17:14 +0800

    Progress: Finish Reward Model

--- app/Models/Reward.php
+++ app/Models/Reward.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reward extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function reward_transaction() {
+        return $this->hasMany(TransactionReward::class, 'id');
+    }
 }
