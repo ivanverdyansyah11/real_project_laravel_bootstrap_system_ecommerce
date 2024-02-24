4abaf21069cbccc33bb4a212c5766a41b82ab733
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:17:27 +0800

    Progress: Finish Transaction Reward Model

---
+++ app/Models/TransactionReward.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TransactionReward extends Model
+{
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function reward() {
+        return $this->belongsTo(Reward::class, 'rewards_id');
+    }
+
+    public function reseller() {
+        return $this->belongsTo(Reseller::class, 'resellers_id');
+    }
+}
