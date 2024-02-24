d7414d5d5dd444bc1fde3e41d58b00f5d6813751
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:15:33 +0800

    Progress: Finish Store Customer Request

---
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email:dns|string',
+            'password' => 'required|string|min:3',
+            'role' => 'required|string',
+            'status' => 'required|integer',
+            'image' => 'required|file|image|mimes:png,jpg,jpeg,gif,webp,svg|max:2048',
+            'name' => 'required|string|max:255',
+            'number_phone' => 'required|string|max:15',
+            'photo_ktp' => 'required|file|image|mimes:png,jpg,jpeg,gif,webp,svg|max:2048',
+        ];
+    }
+}
