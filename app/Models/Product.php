e940cde93bce2fc25e37f2e701fd96448afc7225
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:17:00 +0800

    Progress: Finish Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function category() {
+        return $this->belongsTo(Category::class, 'categories_id');
+    }
+
+    public function transaction() {
+        return $this->hasMany(Transaction::class, 'id');
+    }
 }
