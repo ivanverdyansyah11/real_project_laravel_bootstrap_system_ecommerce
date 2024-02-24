a25a7bc61308c2ebd20132e12fd1ffd73eac282a
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:16:49 +0800

    Progress: Finish Customer Model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+
+    public function transaction() {
+        return $this->hasMany(Transaction::class, 'id');
+    }
 }
