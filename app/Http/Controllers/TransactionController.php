d65b9ca43c8f5e1147a68b844d9601edf1f7d97c
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:14:29 +0800

    Progress: Make Function Index Transaction Controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route as FacadesRoute;
 
 class TransactionController extends Controller
 {
     public function index() : View {
-        return view('transaction.index', [
-            'title' => 'Transaction Page',
-        ]);
+        if (FacadesRoute::is('transaction-pending')) {
+            return view('transaction-pending.index', [
+                'title' => 'Halaman Transaksi Pesanan',
+            ]);
+        } elseif(FacadesRoute::is('transaction-finish')) {
+            return view('transaction-finish.index', [
+                'title' => 'Halaman Transaksi Selesai',
+            ]);
+        } else {
+            return view('report-transaction.index', [
+                'title' => 'Halaman Rekap Transaksi',
+            ]);
+        }
     }
 }
