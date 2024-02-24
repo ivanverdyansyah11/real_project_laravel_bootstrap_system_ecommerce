021c24d853004257b92007bcbcc646324af89ecf
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:22:00 +0800

    Progress: Update Script JS

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,11 @@
 const buttonDataUser = document.querySelector('#data_user');
+const buttonDataReport = document.querySelector('#data_report');
 const buttonDataTransaction = document.querySelector('#data_transaction');
 const childDataUser = document.querySelector('#child_data_user');
+const childDataReport = document.querySelector('#child_data_report');
 const childDataTransaction = document.querySelector('#child_data_transaction');
+const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
 
 if (buttonDataUser) {
     buttonDataUser.addEventListener('click', function() {
@@ -10,9 +14,22 @@ if (buttonDataUser) {
     });
 }
 
+if (buttonDataReport) {
+    buttonDataReport.addEventListener('click', function() {
+        buttonDataReport.classList.toggle('active');
+        childDataReport.classList.toggle('active');
+    });
+}
+
 if (buttonDataTransaction) {
     buttonDataTransaction.addEventListener('click', function() {
         buttonDataTransaction.classList.toggle('active');
         childDataTransaction.classList.toggle('active');
+    });
+}
+
+if (hamburger) {
+    hamburger.addEventListener('click', function() {
+        sidebar.classList.toggle('show');
     });
 }
