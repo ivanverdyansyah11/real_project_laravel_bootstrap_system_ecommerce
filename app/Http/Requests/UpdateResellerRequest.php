390ad69486090f62137190109367b84411cf4b18
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:16:24 +0800

    Progress: Finish Update Reseller Request

---
+++ app/Http/Requests/UpdateResellerRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateResellerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email:dns|string',
+            'password' => 'nullable|string|min:3',
+            'image' => 'nullable|file|image|mimes:png,jpg,jpeg,gif,webp,svg|max:2048',
+            'name' => 'required|string|max:255',
+            'number_phone' => 'required|string|max:15',
+            'photo_ktp' => 'nullable|file|image|mimes:png,jpg,jpeg,gif,webp,svg|max:2048',
+        ];
+    }
+}
