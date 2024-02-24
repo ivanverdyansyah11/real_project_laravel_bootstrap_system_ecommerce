1fc27e55fa7703b0e09f71f73d6f7cd05e7c3b35
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-25 00:16:42 +0800

    Progress: Finish Category Model

---
+++ app/Models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function products() {
+        return $this->hasMany(Product::class, 'id');
+    }
+}
