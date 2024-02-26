df6d811b3155a63cefe0676d500cefb5254faae9
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-26 23:30:22 +0800

    Progress; Finish Auth Repository

--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -2,50 +2,51 @@
 
 namespace App\Repositories;
 
+use App\Models\Customer;
 use App\Models\Reseller;
 use App\Models\User;
 use App\Utils\UploadFile;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
-class AuthRepository 
+class AuthRepository
 {
-  public function __construct(
-    protected readonly User $user,
-    protected readonly Reseller $reseller,
-    protected readonly UploadFile $uploadFile,
-  ) {}
-  
-  public function createUser($request) : Reseller
-  {
-    DB::beginTransaction();
-    try {
-      $filename = $this->uploadFile->uploadSingleFile($request['photo_ktp'], "assets/images/reseller");
-      $request['password'] = bcrypt($request->password);
-      $this->user->create($request->only('email','password','role'));
-      $user = $this->user->latest()->first();
-      $request['users_id'] = $user->id;
-      $reseller = $this->reseller->create([
-        'users_id' => $request['users_id'],
-        'name' => $request['name'],
-        'number_phone' => $request['number_phone'],
-        'photo_ktp' => $filename,
-      ]);
+    public function __construct(
+        protected readonly User $user,
+        protected readonly Reseller $reseller,
+        protected readonly Customer $customer,
+        protected readonly UploadFile $uploadFile,
+    ) {}
 
-      DB::commit();
-      return $reseller;
-    } catch (\Exception $e) {
-      logger($e->getMessage());
-      DB::rollBack();
-      throw $e;
+    public function createUser($request) : User
+    {
+        DB::beginTransaction();
+        try {
+            $filenameKTP = $this->uploadFile->uploadSingleFile($request['photo_ktp'], "assets/images/reseller");
+            $request['photo_ktp'] = $filenameKTP;
+            $request['password'] = bcrypt($request['password']);
+            $userValidation = $this->user->create(Arr::only($request, ['email', 'password', 'role']));
+            $userLastId = $this->user->latest()->first();
+            $request['users_id'] = $userLastId->id;
+            if ($request['role'] == 'reseller') {
+                $this->reseller->create(Arr::only($request, ['users_id', 'name', 'number_phone', 'photo_ktp']));
+            } else {
+                $this->customer->create(Arr::only($request, ['users_id', 'name', 'number_phone', 'photo_ktp']));
+            }
+            DB::commit();
+            return $userValidation;
+        } catch (\Exception $e) {
+            logger($e->getMessage());
+            DB::rollBack();
+            throw $e;
+        }
     }
-  }
 
-  public function logout($request): bool
-  {
-    auth()->logout();
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
-    return true;
-  }
+    public function logout($request): bool
+    {
+        auth()->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return true;
+    }
 }
