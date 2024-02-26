7ae264a5585bc8426042bd4fd008fd62a6de4c86
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-26 23:31:51 +0800

    Progress; Finish Index Page Customer

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -3,9 +3,18 @@
 @section('content-dashboard')
     <div class="row me-lg-4" style="margin-top: 32px;">
         <div class="col-12 pe-lg-0">
-            <a href="#" class="d-none d-md-inline-block button-primary mb-2">Buat Pelanggan</a>
+            @if (session()->has('success'))
+                <div class="alert alert-success w-100 mb-4" role="alert">
+                    {{ session('success') }}
+                </div>
+            @elseif(session()->has('failed'))
+                <div class="alert alert-danger w-100 mb-4" role="alert">
+                    {{ session('failed') }}
+                </div>
+            @endif
+            <a href="{{ route('customer.create') }}" class="d-none d-md-inline-block button-primary mb-2">Buat Pelanggan</a>
             <div class="wrapper-table">
-                <table id="table_pelanggan" class="table display responsive nowrap table-striped" style="width:100%">
+                <table id="table_customer" class="table display responsive nowrap table-striped" style="width:100%">
                     <thead>
                         <tr>
                             <th>No.</th>
@@ -16,85 +25,69 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-edit d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/edit.png') }}" alt="Edit Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-delete d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/delete.png') }}" alt="Delete Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Garrett Winters</td>
-                            <td>Accountant</td>
-                            <td>Tokyo</td>
-                            <td>63</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-edit d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/edit.png') }}" alt="Edit Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-delete d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/delete.png') }}" alt="Delete Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Ashton Cox</td>
-                            <td>Junior Technical Author</td>
-                            <td>San Francisco</td>
-                            <td>66</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-edit d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/edit.png') }}" alt="Edit Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-delete d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/delete.png') }}" alt="Delete Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Cedric Kelly</td>
-                            <td>Senior Javascript Developer</td>
-                            <td>Edinburgh</td>
-                            <td>22</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-edit d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/edit.png') }}" alt="Edit Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-delete d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/delete.png') }}" alt="Delete Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
+                        @if ($customers->count() == 0)
+                            <tr>
+                                <td colspan="6">Data pelanggan tidak ditemukan!</td>
+                            </tr>
+                        @else
+                            @foreach ($customers as $customer)
+                                <tr>
+                                    <td>{{ $customer->id }}</td>
+                                    <td>{{ $customer->name }}</td>
+                                    <td>{{ $customer->user->email }}</td>
+                                    <td>{{ $customer->number_phone }}</td>
+                                    <td class="wrapper d-flex gap-2">
+                                        <a href="{{ route('customer.show', $customer->id) }}" class="button-detail d-flex align-items-center justify-content-center">
+                                            <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
+                                        </a>
+                                        <a href="{{ route('customer.edit', $customer->id) }}" class="button-edit d-none d-md-flex align-items-center justify-content-center">
+                                            <img src="{{ asset('assets/images/icons/edit.png') }}" alt="Edit Icon" class="img-fluid" width="16">
+                                        </a>
+                                        <button type="button" class="button-delete d-none d-md-flex align-items-center justify-content-center" data-bs-toggle="modal" data-bs-target="#deleteModal" data-id="{{ $customer->id }}">
+                                            <img src="{{ asset('assets/images/icons/delete.png') }}" alt="Delete Icon" class="img-fluid" width="16">
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
 
+    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="deleteModalLabel">Modal Hapus Pelanggan</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form id="buttonDeleteCustomer" method="POST" class="d-inline-block w-100">
+                    @csrf
+                    @method("DELETE")
+                    <div class="modal-body">
+                        <p>Apakah kamu yakin untuk menghapus pelanggan ini?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Delete</button>
+                        <button type="submit" class="btn btn-primary">Hapus Pelanggan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
     @push('js')
         <script>
-            $('#table_pelanggan').DataTable( {
+            $('#table_customer').DataTable( {
                 responsive: true
             } );
+
+            $(document).on('click', '[data-bs-target="#deleteModal"]', function() {
+                let id = $(this).data('id');
+                $('#buttonDeleteCustomer').attr('action', '/customer/' + id);
+            });
         </script>
     @endpush
 @endsection
