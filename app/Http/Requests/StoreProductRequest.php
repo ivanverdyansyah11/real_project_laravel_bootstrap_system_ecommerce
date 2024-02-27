a0865e30242df355f8a5aa000d0eb5f94f7577d3
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-27 23:08:41 +0800

    Progress; Finish Store Product Request

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -22,10 +22,12 @@ class StoreProductRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'categories_id' => 'required|integer',
             'image' => 'required|file|image|mimes:png,jpg,jpeg,gif,webp,svg|max:2048',
             'name' => 'required|string|max:255',
             'description' => 'required|string',
             'unit' => 'required|string',
+            'stock' => 'required|integer',
             'purchase_price' => 'required|integer',
             'selling_price' => 'required|integer',
         ];
