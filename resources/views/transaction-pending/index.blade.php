4a2868c35d61dc9bc8d9fc6031bce5cbb283733e
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-27 23:20:19 +0800

    Progress; Finish Index Page Transaction Pending

--- resources/views/transaction-pending/index.blade.php
+++ resources/views/transaction-pending/index.blade.php
@@ -3,98 +3,53 @@
 @section('content-dashboard')
     <div class="row me-lg-4" style="margin-top: 32px;">
         <div class="col-12 pe-lg-0">
+            @if (session()->has('success'))
+                <div class="alert alert-success w-100 mb-4" role="alert">
+                    {{ session('success') }}
+                </div>
+            @elseif(session()->has('failed'))
+                <div class="alert alert-danger w-100 mb-4" role="alert">
+                    {{ session('failed') }}
+                </div>
+            @endif
             <div class="wrapper-table">
                 <table id="table_transaction" class="table display responsive nowrap table-striped" style="width:100%">
                     <thead>
                         <tr>
-                            <th>Nomor Invoice</th>
-                            <th>Nama Barang</th>
-                            <th>Status</th>
+                            <th>Nomor Invois</th>
+                            <th>Nama Pelanggan</th>
+                            <th>Nama Karyawan</th>
+                            <th>Nama Produk</th>
                             <th>Total</th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>75647656756</td>
-                            <td>System Architect</td>
-                            <td>Pending</td>
-                            <td>Rp. 500.000</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-approved d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/approved.png') }}" alt="Approved Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-edit d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/edit.png') }}" alt="Edit Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-delete d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/delete.png') }}" alt="Delete Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>75647656756</td>
-                            <td>System Architect</td>
-                            <td>Pending</td>
-                            <td>Rp. 500.000</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-approved d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/approved.png') }}" alt="Approved Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-edit d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/edit.png') }}" alt="Edit Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-delete d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/delete.png') }}" alt="Delete Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>75647656756</td>
-                            <td>System Architect</td>
-                            <td>Pending</td>
-                            <td>Rp. 500.000</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-approved d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/approved.png') }}" alt="Approved Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-edit d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/edit.png') }}" alt="Edit Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-delete d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/delete.png') }}" alt="Delete Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>75647656756</td>
-                            <td>System Architect</td>
-                            <td>Pending</td>
-                            <td>Rp. 500.000</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-approved d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/approved.png') }}" alt="Approved Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-edit d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/edit.png') }}" alt="Edit Icon" class="img-fluid" width="16">
-                                </a>
-                                <a href="#" class="button-delete d-none d-md-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/delete.png') }}" alt="Delete Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
+                        @if ($transactions->count() == 0)
+                            <tr>
+                                <td>Data transaksi tidak ditemukan!</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        @else
+                            @foreach ($transactions as $transaction)
+                                <tr>
+                                    <td>{{ $transaction->invois }}</td>
+                                    <td>{{ $transaction->customer->name }}</td>
+                                    <td>{{ $transaction->reseller->name }}</td>
+                                    <td>{{ $transaction->product->name }}</td>
+                                    <td>Rp. {{ number_format($transaction->total_payment, 2, ",", ".") }}</td>
+                                    <td class="wrapper d-flex gap-2">
+                                        <a href="{{ route('transaction.show', $transaction->id) }}" class="button-detail d-flex align-items-center justify-content-center">
+                                            <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
                     </tbody>
                 </table>
             </div>
