f393e315571379254801f977572d3a70f712c522
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-27 23:12:14 +0800

    Progress; Update Product Seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,8 +19,9 @@ class ProductSeeder extends Seeder
             'name' => 'Minyak Dewantari',
             'description' => 'Minyak Dewantari adalah produk perawatan kulit eksklusif yang menggabungkan bahan alami berkualitas tinggi, seperti minyak esensial lavender, chamomile, dan tea tree, untuk memberikan perawatan mendalam dan menyeluruh. Diperkaya dengan ekstrak herbal pilihan, produk ini tidak hanya memberikan kelembapan optimal tetapi juga menawarkan aroma menenangkan yang merelaksasi. Cocok untuk semua jenis kulit, Minyak Dewantari memberikan pengalaman perawatan kulit yang unik dan menyegarkan, sambil menekankan keberlanjutan dengan kemasan elegan dan desain ramah lingkungan. Produk ini tidak hanya mendukung kecantikan alami tetapi juga merayakan perpaduan antara keajaiban alam dan teknologi modern dalam merawat kulit Anda.',
             'unit' => 'box',
-            'purchase_price' => '135000',
-            'selling_price' => '135000',
+            'stock' => 350,
+            'purchase_price' => 135000,
+            'selling_price' => 135000,
         ]);
 
         Product::create([
@@ -29,8 +30,9 @@ class ProductSeeder extends Seeder
             'name' => 'Ramuan Diksa',
             'description' => 'Ramuan Diksa adalah ramuan alami yang memadukan kekayaan rempah-rempah pilihan untuk memberikan pengalaman kesehatan dan kebugaran yang holistik. Diracik dengan cermat menggunakan bahan-bahan herbal tradisional, Ramuan Diksa menawarkan manfaat positif bagi tubuh dan jiwa. Kombinasi unik dari jahe, kunyit, temulawak, dan rempah-rempah lainnya memberikan dukungan untuk meningkatkan daya tahan tubuh, memperkuat sistem pencernaan, dan merangsang energi vital. Ramuan ini tidak hanya menyajikan rasa yang lezat tetapi juga memberikan keharmonisan bagi tubuh secara keseluruhan. Ramuan Diksa cocok untuk digunakan sebagai suplemen kesehatan sehari-hari, membawa Anda pada perjalanan kesehatan yang alami dan menyeluruh. Temukan keajaiban ramuan tradisional dengan Ramuan Diksa, pilihan terbaik untuk meningkatkan kesehatan Anda secara alami.',
             'unit' => 'pcs',
-            'purchase_price' => '135000',
-            'selling_price' => '55000',
+            'stock' => 210,
+            'purchase_price' => 135000,
+            'selling_price' => 55000,
         ]);
 
         Product::create([
@@ -39,8 +41,9 @@ class ProductSeeder extends Seeder
             'name' => 'Kapsul Visnhu',
             'description' => 'Kapsul Vishnu adalah inovasi terkini dalam perawatan kesehatan yang menyajikan solusi holistik untuk meningkatkan kesejahteraan Anda. Dirancang dengan cermat menggunakan formula yang terdiri dari bahan-bahan alami pilihan, Kapsul Vishnu menggabungkan kebijaksanaan warisan tradisional dengan kemajuan ilmiah modern. Kapsul ini mengandung campuran unik vitamin, mineral, dan ekstrak tumbuhan yang secara sinergis bekerja untuk mendukung sistem kekebalan tubuh, meningkatkan energi, dan menjaga keseimbangan nutrisi. Dengan konsep yang berfokus pada keharmonisan tubuh dan pikiran, Kapsul Vishnu tidak hanya memberikan manfaat kesehatan fisik tetapi juga mendukung kesejahteraan mental Anda. Ambil langkah menuju gaya hidup sehat dan seimbang dengan Kapsul Vishnu, pilihan terbaik untuk merawat tubuh secara menyeluruh.',
             'unit' => 'pcs',
-            'purchase_price' => '135000',
-            'selling_price' => '100000',
+            'stock' => 12,
+            'purchase_price' => 135000,
+            'selling_price' => 100000,
         ]);
     }
 }
