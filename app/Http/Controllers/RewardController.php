1a8a767954745965fa173225b79ade8798a4e205
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-27 23:08:04 +0800

    Progress; Finish Reward Controller

--- app/Http/Controllers/RewardController.php
+++ app/Http/Controllers/RewardController.php
@@ -2,14 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreRewardRequest;
+use App\Http\Requests\UpdateRewardRequest;
+use App\Models\Reward;
+use App\Repositories\RewardRepositories;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 
 class RewardController extends Controller
 {
+    public function __construct(
+        protected readonly RewardRepositories $reward,
+    ) {}
+
     public function index() : View {
         return view('reward.index', [
             'title' => 'Halaman Penghargaan',
+            'rewards' => $this->reward->findAllPaginate(),
         ]);
+    }
+
+    public function show(Reward $reward) : JsonResponse {
+        try {
+            return response()->json([
+                'status' => 'success',
+                'data' => $this->reward->findById($reward->id),
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Gagal untuk mengambil data dari penghargaan dengan ID ' . $reward->id,
+            ], 500);
+        }
+    }
+
+    public function store(StoreRewardRequest $request) : RedirectResponse {
+        try {
+            $this->reward->store($request->validated());
+            return redirect(route('reward.index'))->with('success', 'Berhasil menambahkan penghargaan baru!');
+        } catch (\Exception $e) {
+            logger($e->getMessage());
+            return redirect(route('reward.index'))->with('failed', 'Gagal menambahkan penghargaan baru!');
+        }
+    }
+
+    public function update(UpdateRewardRequest $request, Reward $reward) : RedirectResponse {
+        try {
+            $this->reward->update($request->validated(), $reward);
+            return redirect(route('reward.index'))->with('success', 'Berhasil edit penghargaan!');
+        } catch (\Exception $e) {
+            return redirect(route('reward.index'))->with('failed', 'Gagal edit penghargaan!');
+        }
+    }
+
+    public function destroy(Reward $reward) : RedirectResponse {
+        try {
+            $this->reward->delete($reward);
+            return redirect(route('reward.index'))->with('success', 'Berhasil hapus penghargaan!');
+        } catch (\Exception $e) {
+            return redirect(route('reward.index'))->with('failed', 'Gagal hapus penghargaan!');
+        }
     }
 }
