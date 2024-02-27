c20917c734365f21fdafc2336d6901f7084e96cc
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 01:18:18 +0800

    Progress; Finish Update Admin Migration

---
+++ database/migrations/2024_02_27_223615_create_admins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('users_id');
+            $table->string('photo_ktp');
+            $table->string('name', 255);
+            $table->string('number_phone', 15);
+            $table->string('origin', 50)->nullable();
+            $table->string('place_of_birth', 50)->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('gender', 10)->nullable();
+            $table->text('address')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
