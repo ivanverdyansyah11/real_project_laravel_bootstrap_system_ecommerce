b638fb789a06005c5a11ed9b17c316168f8caa5c
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 01:17:11 +0800

    Progress; Finish Model Admin

---
+++ app/Models/Admin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Admin extends Model
+{
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+}
