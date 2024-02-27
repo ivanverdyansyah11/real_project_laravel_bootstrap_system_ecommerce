6b1a2e7b7ace433ee5fdfe3f82c2ee36578db8bf
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-27 23:08:47 +0800

    Progress; Finish Store Transaction Request

---
+++ app/Http/Requests/StoreTransactionRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'products_id' => 'required|integer',
+            'customers_id' => 'required|integer',
+            'resellers_id' => 'nullable|integer',
+            'quantity' => 'required|integer',
+            'total' => 'required|integer',
+            'total_payment' => 'required|integer',
+            'status' => 'nullable|integer',
+        ];
+    }
+}
