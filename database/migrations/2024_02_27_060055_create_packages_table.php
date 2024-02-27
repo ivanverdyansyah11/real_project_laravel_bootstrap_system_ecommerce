24cbd4856605c45b6bc54d28f58f6c8068661622
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-27 23:11:43 +0800

    Progress; Finish Package Migration

---
+++ database/migrations/2024_02_27_060055_create_packages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('products_id')->index();
+            $table->string('name', 255)->nullable();
+            $table->integer('quantity');
+            $table->integer('selling_price');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('packages');
+    }
+};
