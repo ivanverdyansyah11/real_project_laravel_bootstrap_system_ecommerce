d5f5c95efeaa51bb7d602aa189c3e732e04caf79
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-27 23:07:25 +0800

    Progress; Finish Cashier Controller

--- app/Http/Controllers/CashierController.php
+++ app/Http/Controllers/CashierController.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\CustomerRepositories;
+use App\Repositories\ProductRepositories;
+use App\Repositories\ResellerRepositories;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class CashierController extends Controller
 {
+    public function __construct(
+        protected readonly CustomerRepositories $customer,
+        protected readonly ResellerRepositories $reseller,
+        protected readonly ProductRepositories $product,
+    ) {}
+
     public function index() : View {
         return view('cashier.index', [
             'title' => 'Halaman Kasir',
+            'customers' => $this->customer->findAll(),
+            'resellers' => $this->reseller->findAll(),
+            'products' => $this->product->findAll(),
         ]);
     }
 }
