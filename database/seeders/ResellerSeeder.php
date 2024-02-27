73298bdc03f3ab238d9aa62a345fa5ecdfe45f73
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 01:18:50 +0800

    Progress; Finish Update Reseller Seeder

--- database/seeders/ResellerSeeder.php
+++ database/seeders/ResellerSeeder.php
@@ -15,16 +15,26 @@ class ResellerSeeder extends Seeder
     {
         Reseller::create([
             'users_id' => 3,
+            'photo_ktp' => 'photo_ktp_3.jpg',
             'name' => 'Agus Wartawan',
             'number_phone' => '08123456789',
-            'photo_ktp' => 'photo_ktp_1.jpg',
+            'origin' => 'Singaraja',
+            'place_of_birth' => 'badung',
+            'date_of_birth' => '2004-06-13',
+            'gender' => 'L',
+            'address' => 'Jl. Veteran',
         ]);
 
         Reseller::create([
             'users_id' => 4,
+            'photo_ktp' => 'photo_ktp_4.jpg',
             'name' => 'Ayu Putri',
             'number_phone' => '08987654321',
-            'photo_ktp' => 'photo_ktp_2.jpg',
+            'origin' => 'Denpasar',
+            'place_of_birth' => 'Denpasar',
+            'date_of_birth' => '2003-11-09',
+            'gender' => 'P',
+            'address' => 'Jl. Grand Corry',
         ]);
     }
 }
