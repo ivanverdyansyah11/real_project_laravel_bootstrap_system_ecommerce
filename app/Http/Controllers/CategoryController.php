5fd11e23a4c3024333631e566f7cbf0d6323536f
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-27 23:07:35 +0800

    Progress; Finish Category Controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,38 +4,64 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+use App\Repositories\CategoryRepositories;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 
 class CategoryController extends Controller
 {
+    public function __construct(
+        protected readonly CategoryRepositories $category,
+    ) {}
+
     public function index() : View {
         return view('category.index', [
             'title' => 'Halaman Kategori',
+            'categories' => $this->category->findAllPaginate(),
         ]);
     }
 
-    public function create() : View {
-        return view('category.add', [
-            'title' => 'Halaman Tambah Kategori',
-        ]);
+    public function show(Category $category) : JsonResponse {
+        try {
+            return response()->json([
+                'status' => 'success',
+                'data' => $this->category->findById($category->id),
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Gagal untuk mengambil data dari kategori dengan ID ' . $category->id,
+            ], 500);
+        }
     }
 
-    public function store(StoreCategoryRequest $request) {
-        dd($request->all());
+    public function store(StoreCategoryRequest $request) : RedirectResponse {
+        try {
+            $this->category->store($request->validated());
+            return redirect(route('category.index'))->with('success', 'Berhasil menambahkan kategori baru!');
+        } catch (\Exception $e) {
+            logger($e->getMessage());
+            return redirect(route('category.index'))->with('failed', 'Gagal menambahkan kategori baru!');
+        }
     }
 
-    public function edit() : View {
-        return view('reseller.edit', [
-            'title' => 'Halaman Edit Kategori',
-        ]);
+    public function update(UpdateCategoryRequest $request, Category $category) : RedirectResponse {
+        try {
+            $this->category->update($request->validated(), $category);
+            return redirect(route('category.index'))->with('success', 'Berhasil edit kategori!');
+        } catch (\Exception $e) {
+            return redirect(route('category.index'))->with('failed', 'Gagal edit kategori!');
+        }
     }
 
-    public function update(UpdateCategoryRequest $request) {
-        dd($request->all());
-    }
-
-    public function destroy($id) {
-        dd($id);
+    public function destroy(Category $category) : RedirectResponse {
+        try {
+            $this->category->delete($category);
+            return redirect(route('category.index'))->with('success', 'Berhasil hapus kategori!');
+        } catch (\Exception $e) {
+            return redirect(route('category.index'))->with('failed', 'Gagal hapus kategori!');
+        }
     }
 }
