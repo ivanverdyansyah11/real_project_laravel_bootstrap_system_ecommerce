e12b22c4a1c306019ec5d541fae8f2984acabd23
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-27 23:14:38 +0800

    Progress; Finish Partial Modal Reseller

---
+++ resources/views/partials/reseller.blade.php
@@ -0,0 +1,21 @@
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="deleteModalLabel">Modal Hapus Karyawan</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="buttonDeleteReseller" method="POST" class="d-inline-block w-100">
+                @csrf
+                @method("DELETE")
+                <div class="modal-body">
+                    <p>Apakah kamu yakin untuk menghapus karyawan ini?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Batal Hapus</button>
+                    <button type="submit" class="btn btn-primary">Hapus Karyawan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
