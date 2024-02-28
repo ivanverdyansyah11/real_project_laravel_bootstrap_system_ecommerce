36648295320dba8fc1f9249909cd530d719c77d2
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 15:34:37 +0800

    Progress: Update Update Reward Request

--- app/Http/Requests/UpdateRewardRequest.php
+++ app/Http/Requests/UpdateRewardRequest.php
@@ -21,7 +21,8 @@ class UpdateRewardRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [       
+        return [
+            'image' => 'nullable|file|image|mimes:png,jpg,jpeg,gif,webp,svg|max:2048',
             'name' => 'required|string|max:255',
             'description' => 'required|string',
             'points_required' => 'required|integer',
