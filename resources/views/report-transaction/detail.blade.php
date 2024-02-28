fde1326b7b1fd84754002ed34179218364525008
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 15:36:58 +0800

    Progress: Update Detail Page Report Transaction

--- resources/views/report-transaction/detail.blade.php
+++ resources/views/report-transaction/detail.blade.php
@@ -10,36 +10,40 @@
             @endif
         </div>
         <div class="col-12 pe-lg-0">
-            <form class="row row-cols-1 row-cols-md-2">
-                <div class="col mb-3">
+            <form class="row">
+                <div class="col-12 mb-3 d-flex flex-column">
+                    <label for="proof_of_payment" class="form-label">Foto Bukti Pembayaran</label>
+                    <img src="{{ $transaction->proof_of_payment ? asset('assets/images/transaction/' . $transaction->proof_of_payment) : asset('assets/images/other/img-not-found.jpg') }}" alt="Image Not Found" class="rounded mb-2 img-preview" width="100" height="100" style="object-fit: cover;">
+                </div>
+                <div class="col-md-6 mb-3">
                     <label for="customers_id" class="form-label">Nama Pelanggan</label>
                     <input readonly type="text" class="form-control" id="customers_id" value="{{ $transaction->customer->name }}">
                 </div>
-                <div class="col mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="resellers_id" class="form-label">Nama Karyawan</label>
-                    <input readonly type="text" class="form-control" id="resellers_id" value="{{ $transaction->reseller->name }}">
+                    <input readonly type="text" class="form-control" id="resellers_id" value="{{ $transaction->reseller ? $transaction->reseller->name : '-' }}">
                 </div>
-                <div class="col mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="products_id" class="form-label">Nama Produk</label>
                     <input readonly type="text" class="form-control" id="products_id" value="{{ $transaction->product->name }}">
                 </div>
-                <div class="col mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="quantity" class="form-label">Kuantitas Dibeli</label>
                     <input readonly type="number" class="form-control" id="quantity" value="{{ $transaction->quantity }}">
                 </div>
-                <div class="col mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="price_per_unit" class="form-label">Harga Satuan</label>
                     <input readonly type="text" class="form-control" id="price_per_unit" value="Rp. {{ number_format($transaction->product->selling_price, 2, ",", ".") }}">
                 </div>
-                <div class="col mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="total" class="form-label">Total</label>
                     <input readonly type="text" class="form-control" id="total" value="Rp. {{ number_format($transaction->total, 2, ",", ".") }}">
                 </div>
-                <div class="col mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="total_payment" class="form-label">Total Bayar</label>
                     <input readonly type="text" class="form-control" id="total_payment" value="Rp. {{ number_format($transaction->total_payment, 2, ",", ".") }}">
                 </div>
-                <div class="col mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="created_at" class="form-label">Transaksi Dilakukan</label>
                     <input readonly type="text" class="form-control" id="created_at" value="{{ Carbon\Carbon::parse($transaction->created_at)->format('l, d F Y') }}">
                 </div>
