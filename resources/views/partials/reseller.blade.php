56134f8d51f4f34ddd3ee4eebd93db868e9b63b8
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 22:48:44 +0800

    Progress: Update Reseller Modal Partial

--- resources/views/partials/reseller.blade.php
+++ resources/views/partials/reseller.blade.php
@@ -19,3 +19,25 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="approveModal" tabindex="-1" aria-labelledby="approveModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="approveModalLabel">Modal Setujui Karyawan</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="buttonApprovedReseller" method="POST" class="d-inline-block w-100">
+                @csrf
+                @method("PUT")
+                <div class="modal-body">
+                    <p>Apakah kamu yakin untuk menyetujui karyawan ini?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Batal Setujui</button>
+                    <button type="submit" class="btn btn-primary">Setujui Karyawan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
