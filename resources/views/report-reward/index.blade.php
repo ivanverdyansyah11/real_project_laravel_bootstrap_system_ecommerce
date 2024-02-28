d8c2411512d3c4e8732a8a941339c58cbe613a2f
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 22:49:36 +0800

    Progress: Update Index Page Report Reward

--- resources/views/report-reward/index.blade.php
+++ resources/views/report-reward/index.blade.php
@@ -16,58 +16,42 @@
                 <table id="table_report" class="table display responsive nowrap table-striped" style="width:100%">
                     <thead>
                         <tr>
-                            <th>Nomor Invoice</th>
-                            <th>Nama Barang</th>
-                            <th>Disetujui Oleh</th>
-                            <th>Total</th>
+                            <th>No.</th>
+                            <th>Nama Karyawan</th>
+                            <th>Nama Penghargaan</th>
+                            <th>Poin Dibutuhkan</th>
+                            <th>Tanggal Ditukarnya</th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>75647656756</td>
-                            <td>System Architect</td>
-                            <td>Aditya Prayatna</td>
-                            <td>Rp. 500.000</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>75647656756</td>
-                            <td>System Architect</td>
-                            <td>Aditya Prayatna</td>
-                            <td>Rp. 500.000</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>75647656756</td>
-                            <td>System Architect</td>
-                            <td>Aditya Prayatna</td>
-                            <td>Rp. 500.000</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>75647656756</td>
-                            <td>System Architect</td>
-                            <td>Aditya Prayatna</td>
-                            <td>Rp. 500.000</td>
-                            <td class="wrapper d-flex gap-2">
-                                <a href="#" class="button-detail d-flex align-items-center justify-content-center">
-                                    <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
-                                </a>
-                            </td>
-                        </tr>
+                        @if ($transactions->count() == 0)
+                            <tr>
+                                <tr>
+                                    <td>Data transaksi tidak ditemukan!</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tr>
+                        @else
+                            @foreach ($transactions as $i => $transaction)
+                                <tr>
+                                    <td>{{ $i + 1 }}</td>
+                                    <td>{{ $transaction->reseller->name }}</td>
+                                    <td>{{ $transaction->reward->name }}</td>
+                                    <td>{{ $transaction->reward->points_required }}</td>
+                                    <td>{{ Carbon\Carbon::parse($transaction->created_at)->format('l, d F Y') }}</td>
+                                    <td class="wrapper d-flex gap-2">
+                                        <a href="{{ route('report-reward.show', $transaction->id) }}" class="button-detail d-flex align-items-center justify-content-center">
+                                            <img src="{{ asset('assets/images/icons/detail.png') }}" alt="Detail Icon" class="img-fluid" width="16">
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
                     </tbody>
                 </table>
             </div>
