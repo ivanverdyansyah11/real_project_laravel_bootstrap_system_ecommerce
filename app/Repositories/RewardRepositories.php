6980e7ac8f5a26dd45caeab6a1e22df6dcf14453
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 15:34:49 +0800

    Progress: Update Reward Repositories

--- app/Repositories/RewardRepositories.php
+++ app/Repositories/RewardRepositories.php
@@ -3,11 +3,13 @@
 namespace App\Repositories;
 
 use App\Models\Reward;
+use App\Utils\UploadFile;
 
 class RewardRepositories
 {
     public function __construct(
-        protected readonly Reward $reward
+        protected readonly Reward $reward,
+        protected readonly UploadFile $uploadFile,
     ) {}
 
     public function findAll()
@@ -27,16 +29,25 @@ class RewardRepositories
 
     public function store($request): reward
     {
+        $request['image'] = $this->uploadFile->uploadSingleFile($request['image'], "assets/images/reward");
         return $this->reward->create($request);
     }
 
     public function update($request, $reward): bool
     {
+        if (isset($request["image"])) {
+            $this->uploadFile->deleteExistFile("assets/images/reward/" . $reward->image);
+            $filename = $this->uploadFile->uploadSingleFile($request['image'], 'assets/images/reward');
+            $request['image'] = $filename;
+        } else {
+            $request['image'] = $reward->image;
+        }
         return $reward->update($request);
     }
 
     public function delete($reward): bool
     {
+        $this->uploadFile->deleteExistFile("assets/images/reward/" . $reward->image);
         return $reward->delete();
     }
 }
