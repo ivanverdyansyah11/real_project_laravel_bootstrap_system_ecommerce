5b41ebf7b4a56d3c413295fc0f629ea84950007b
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 18:37:07 +0800

    Progress: Update Profile Repositories

--- app/Repositories/ProfileRepositories.php
+++ app/Repositories/ProfileRepositories.php
@@ -15,7 +15,7 @@ class ProfileRepositories
     public function __construct(
         protected readonly User $user,
         protected readonly Admin $admin,
-        protected readonly Reseller $reseller,
+        protected readonly ResellerRepositories $reseller,
         protected readonly Customer $customer,
         protected readonly UploadFile $uploadFile,
     ) {}
@@ -25,6 +25,9 @@ class ProfileRepositories
         DB::beginTransaction();
         try {
             $user = $this->user->where('email', $request['email'])->first();
+            if ($user['role'] == 'reseller') {
+                $reseller = $this->reseller->findByUserId($user->id);
+            }
             if ($user['role'] == 'super_admin' || $user['role'] == 'admin') {
                 if (isset($request["photo_ktp"])) {
                     $this->uploadFile->deleteExistFile("assets/images/admin/" . $user->admin->photo_ktp);
@@ -35,11 +38,11 @@ class ProfileRepositories
                 }
             } elseif($user['role'] == 'reseller') {
                 if (isset($request["photo_ktp"])) {
-                    $this->uploadFile->deleteExistFile("assets/images/reseller/" . $user->reseller->photo_ktp);
+                    $this->uploadFile->deleteExistFile("assets/images/reseller/" . $reseller->photo_ktp);
                     $filename = $this->uploadFile->uploadSingleFile($request['photo_ktp'], 'assets/images/reseller');
                     $request['photo_ktp'] = $filename;
                 } else {
-                    $request['photo_ktp'] = $user->reseller->photo_ktp;
+                    $request['photo_ktp'] = $reseller->photo_ktp;
                 }
             } else {
                 if (isset($request["photo_ktp"])) {
@@ -60,7 +63,7 @@ class ProfileRepositories
             if ($user['role'] == 'super_admin' || $user['role'] == 'admin') {
                 $user->admin->update(Arr::except($request, ['email', 'image', 'password', 'confirmation_password']));
             } elseif($user['role'] == 'reseller') {
-                $user->reseller->update(Arr::except($request, ['email', 'image', 'password', 'confirmation_password']));
+                $reseller->update(Arr::except($request, ['email', 'image', 'password', 'confirmation_password']));
             } else {
                 $user->customer->update(Arr::except($request, ['email', 'image', 'password', 'confirmation_password']));
             }
