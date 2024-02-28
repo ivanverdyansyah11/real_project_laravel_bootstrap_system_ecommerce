1a55b7d8ff7fe85957cdb3dc4e78077cabccecb5
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-28 18:36:20 +0800

    Progress: Update Dashboard Controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Repositories\PackageRepositories;
 use App\Repositories\ProductRepositories;
+use App\Repositories\ResellerRepositories;
 use App\Repositories\TransactionRepositories;
 use Illuminate\Contracts\View\View;
 
@@ -12,6 +13,7 @@ class DashboardController extends Controller
     public function __construct(
         protected readonly ProductRepositories $product,
         protected readonly PackageRepositories $package,
+        protected readonly ResellerRepositories $reseller,
         protected readonly TransactionRepositories $transaction,
     ) {}
 
@@ -28,17 +30,16 @@ class DashboardController extends Controller
                 'graphic_month' => $this->transaction->filterMonth(),
             ]);
         } else {
-            return '123';
-            // return view('dashboard.index', [
-            //     'title' => 'Halaman Dashboard',
-            //     'total_product' => count($this->product->findAll()),
-            //     'total_package' => count($this->package->findAll()),
-            //     'total_product_sold' => $this->transaction->findTotalProductSold(),
-            //     'total_revenue' => $this->transaction->findTotalRevenue(),
-            //     'graphic_day' => $this->transaction->filterDay(),
-            //     'graphic_week' => $this->transaction->filterWeek(),
-            //     'graphic_month' => $this->transaction->filterMonth(),
-            // ]);
+            return view('dashboard.index', [
+                'title' => 'Halaman Dashboard',
+                'total_poin' => $this->reseller->findByUserId(auth()->user()->id)->poin,
+                'total_transaction' => count($this->transaction->findAllByReseller(auth()->user()->id)),
+                'total_product_sold' => $this->transaction->findTotalProductSoldByReseller(auth()->user()->id),
+                'total_revenue' => $this->transaction->findTotalRevenueByReseller(auth()->user()->id),
+                'graphic_day' => $this->transaction->filterDayByReseller(auth()->user()->id),
+                'graphic_week' => $this->transaction->filterWeekByReseller(auth()->user()->id),
+                'graphic_month' => $this->transaction->filterMonthByReseller(auth()->user()->id),
+            ]);
         }
     }
 }
