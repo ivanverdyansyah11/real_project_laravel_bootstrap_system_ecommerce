f834440bcc7528a5c329cdeeebf6e6742183e3bd
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-01 00:56:23 +0800

    Progress: Finish Component Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,63 @@
+<div class="container">
+    <footer class="footer">
+        <div class="row">
+            <div class="col-lg-4 col-xl-3">
+                <img src="{{ asset('assets/images/brand/brand-logo.svg') }}" alt="Brand Logo" width="62">
+                <p class="paragraph-small my-3">PT. Adigoero Siwa ambara merupakan suatu perusahaan perusahaan yang berfokus pada bidang perbaikan manusia secara holistik. PT. Adigoeroe Siwa Ambara mulai berdiri pada tahun 2022 dan merupakan salah satu anak perusahaan yang berada di bawah naungan Varash Group</p>
+                <div class="sosmed-link d-flex gap-3 flex-column">
+                    <a href="#" class="d-flex gap-2 align-items-center">
+                        <img src="{{ asset('assets/images/icons/number_phone.svg') }}" alt="Icon Sosmed" width="20">
+                        <span class="paragraph-small">08977-663-6666</span>
+                    </a>
+                    <a href="#" class="d-flex gap-2 align-items-center">
+                        <img src="{{ asset('assets/images/icons/email.svg') }}" alt="Icon Sosmed" width="20">
+                        <span class="paragraph-small">minyakdewantari@gmail.com</span>
+                    </a>
+                    <a href="#" class="d-flex gap-2 align-items-center">
+                        <img src="{{ asset('assets/images/icons/location.svg') }}" alt="Icon Sosmed" width="20">
+                        <span class="paragraph-small">Jalan Ayani, Gg. Sriti Peguyangan, Denpasar utara</span>
+                    </a>
+                </div>
+            </div>
+            <div class="offset-lg-1 offset-xl-2 col-lg-3 d-none d-lg-inline-block">
+                <h6 class="card-body-title">Layanan & Ketentuan</h6>
+                <div class="footer-link d-flex flex-column gap-2 mt-3">
+                    <a href="#" class="link-redirect">Produk</a>
+                    <a href="#" class="link-redirect">Website</a>
+                    <a href="#" class="link-redirect">Kontak kami</a>
+                    <a href="#" class="link-redirect">Mitra</a>
+                    <a href="#" class="link-redirect">Syarat & Ketentuan</a>
+                    <a href="#" class="link-redirect">Kebijakan Privasi</a>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-2 mt-4 mt-lg-0">
+                <h6 class="card-body-title">Didukung Oleh</h6>
+                <div class="footer-link d-flex gap-2 mt-3">
+                    <img src="{{ asset('assets/images/brand/brand-varash.svg') }}" alt="Brand Varash" height="36">
+                    <img src="{{ asset('assets/images/brand/brand-bpom.svg') }}" alt="Brand Bpom" height="36">
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-2 mt-4 mt-lg-0">
+                <h6 class="card-body-title">Ikuti Kami</h6>
+                <div class="footer-link d-flex gap-2 mt-3">
+                    <a href="https://www.facebook.com/" target="_blank">
+                        <img src="{{ asset('assets/images/icons/facebook.svg') }}" alt="Sosmed Icon" height="32">
+                    </a>
+                    <a href="https://www.instagram.com/" target="_blank">
+                        <img src="{{ asset('assets/images/icons/instagram.svg') }}" alt="Sosmed Icon" height="32">
+                    </a>
+                    <a href="https://www.tiktok.com/explore" target="_blank">
+                        <img src="{{ asset('assets/images/icons/tiktok.svg') }}" alt="Sosmed Icon" height="32">
+                    </a>
+                    <a href="https://www.youtube.com/" target="_blank">
+                        <img src="{{ asset('assets/images/icons/youtube.svg') }}" alt="Sosmed Icon" height="32">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="copyright">
+        <p>Adigoeroe ©️2022 All Rights Reserved.</p>
+    </div>
+</div>
