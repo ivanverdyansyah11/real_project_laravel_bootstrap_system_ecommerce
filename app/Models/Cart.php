5fd9c8416ddd5ee35c174ff9d4b6c09158e683a2
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-01 00:54:03 +0800

    Progress: Finish Cart Model

---
+++ app/Models/Cart.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Cart extends Model
+{
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function product() {
+        return $this->belongsTo(Product::class, 'products_id');
+    }
+
+    public function customer() {
+        return $this->belongsTo(Customer::class, 'customers_id');
+    }
+}
