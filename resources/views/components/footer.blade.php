b54f5f9a01fd8df0ac5349b7827c9d08c5c0b041
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-08 00:26:21 -0800

    Progress: Update Component Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,15 +5,15 @@
                 <img src="{{ asset('assets/images/brand/brand-logo.svg') }}" alt="Brand Logo" width="62">
                 <p class="paragraph-small my-3">PT. Adigoero Siwa ambara merupakan suatu perusahaan perusahaan yang berfokus pada bidang perbaikan manusia secara holistik. PT. Adigoeroe Siwa Ambara mulai berdiri pada tahun 2022 dan merupakan salah satu anak perusahaan yang berada di bawah naungan Varash Group</p>
                 <div class="sosmed-link d-flex gap-3 flex-column">
-                    <a href="#" class="d-flex gap-2 align-items-center">
+                    <a target="_blank" href="https://wa.me/089776636666" class="d-flex gap-2 align-items-center">
                         <img src="{{ asset('assets/images/icons/number_phone.svg') }}" alt="Icon Sosmed" width="20">
                         <span class="paragraph-small">08977-663-6666</span>
                     </a>
-                    <a href="#" class="d-flex gap-2 align-items-center">
+                    <a target="_blank" href="mailto:minyakdewantari@gmail.com" class="d-flex gap-2 align-items-center">
                         <img src="{{ asset('assets/images/icons/email.svg') }}" alt="Icon Sosmed" width="20">
                         <span class="paragraph-small">minyakdewantari@gmail.com</span>
                     </a>
-                    <a href="#" class="d-flex gap-2 align-items-center">
+                    <a target="_blank" href="https://maps.app.goo.gl/RBLreHYkFEqYeGt98" class="d-flex gap-2 align-items-center">
                         <img src="{{ asset('assets/images/icons/location.svg') }}" alt="Icon Sosmed" width="20">
                         <span class="paragraph-small">Jalan Ayani, Gg. Sriti Peguyangan, Denpasar utara</span>
                     </a>
@@ -22,12 +22,12 @@
             <div class="offset-lg-1 offset-xl-2 col-lg-3 d-none d-lg-inline-block">
                 <h6 class="card-body-title">Layanan & Ketentuan</h6>
                 <div class="footer-link d-flex flex-column gap-2 mt-3">
-                    <a href="#" class="link-redirect">Produk</a>
-                    <a href="#" class="link-redirect">Website</a>
-                    <a href="#" class="link-redirect">Kontak kami</a>
-                    <a href="#" class="link-redirect">Mitra</a>
-                    <a href="#" class="link-redirect">Syarat & Ketentuan</a>
-                    <a href="#" class="link-redirect">Kebijakan Privasi</a>
+                    <a href="{{ route('products') }}" class="link-redirect">Produk</a>
+                    <a href="{{ route('homepage') }}" class="link-redirect">Website</a>
+                    <a target="_blank" href="https://wa.me/089776636666" class="link-redirect">Kontak kami</a>
+                    <a href="" class="link-redirect">Mitra</a>
+                    <a href="" class="link-redirect">Syarat & Ketentuan</a>
+                    <a href="" class="link-redirect">Kebijakan Privasi</a>
                 </div>
             </div>
             <div class="col-md-6 col-lg-2 mt-4 mt-lg-0">
@@ -58,6 +58,6 @@
     </footer>
 
     <div class="copyright">
-        <p>Adigoeroe ©️2022 All Rights Reserved.</p>
+        <p>Adigoeroe ©️2024 All Rights Reserved.</p>
     </div>
 </div>
