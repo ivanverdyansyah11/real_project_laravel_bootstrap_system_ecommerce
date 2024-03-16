c360f6edd0ab9b37750c6de9842281501d6ea041
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-17 05:33:17 +0800

    Progress: Update Cart Migration

--- database/migrations/2024_02_29_154414_create_carts_table.php
+++ database/migrations/2024_02_29_154414_create_carts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->bigInteger('users_id')->index();
             $table->integer('quantity');
             $table->integer('invois')->nullable();
+            $table->integer('status')->default(1);
             $table->timestamps();
             $table->softDeletes();
         });
