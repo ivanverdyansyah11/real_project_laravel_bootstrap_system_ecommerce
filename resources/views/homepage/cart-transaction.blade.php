127b46dba7ad0242953642f17edd468230ade3ee
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-17 05:34:09 +0800

    Progress: Update Cart Transaction Homepage

--- resources/views/homepage/cart-transaction.blade.php
+++ resources/views/homepage/cart-transaction.blade.php
@@ -86,7 +86,7 @@
                                 @if (auth()->user()->role == 'super_admin' || auth()->user()->role == 'admin')
                                     <div class="col-md-6 mb-3">
                                         <label for="resellers_id" class="form-label">Nama Karyawan</label>
-                                        <select required class="form-control @error('resellers_id') is-invalid @enderror" id="resellers_id" name="resellers_id">
+                                        <select class="form-control @error('resellers_id') is-invalid @enderror" id="resellers_id" name="resellers_id">
                                             <option value="">Pilih karyawan</option>
                                             @foreach ($resellers as $reseller)
                                                 <option value="{{ $reseller->id }}">{{ $reseller->name }}</option>
@@ -105,97 +105,11 @@
                                         <a href="{{ route('cart.index') }}" class="button-dark">Batal Lanjutkan</a>
                                     </div>
                                 </div>
-                                {{-- <div class="col-md-6 mb-3">
-                                    <label for="total" class="form-label">Total</label>
-                                    <input required type="number" class="form-control @error('total') is-invalid @enderror" id="total" name="total" value="{{ old('total', $cart->product->selling_price) }}">
-                                    @error('total')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="total_payment" class="form-label">Total Bayar</label>
-                                    <input required type="number" class="form-control @error('total_payment') is-invalid @enderror" id="total_payment" name="total_payment" value="{{ old('total_payment') }}">
-                                    @error('total_payment')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div> --}}
                             </div>
                         </div>
                     </div>
-                    {{-- <div class="card mt-4">
-                        <div class="card-body">
-                            <h6 class="card-body-title mb-4">Upload Bukti Pembayaran</h6>
-                            <div class="row">
-                                <div class="col-12 mb-3 d-flex flex-column">
-                                    <label for="proof_of_payment" class="form-label">Foto Bukti Pembayaran</label>
-                                    <img src="{{ asset('assets/images/other/img-not-found.jpg') }}" alt="Image Not Found" class="rounded mb-2 img-preview" width="100" height="100" style="object-fit: cover;">
-                                    <input type="file" class="form-control input-file @error('proof_of_payment') is-invalid @enderror" name="proof_of_payment" id="proof_of_payment">
-                                    @error('proof_of_payment')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
-                                <div class="wrapper d-flex gap-2">
-                                    <button type="submit" class="button-primary">Tambah Karyawan</button>
-                                    <a href="{{ route('cart.index') }}" class="button-dark">Batal Tambah</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div> --}}
                 </form>
             </div>
         </div>
     </div>
-
-    @push('js')
-        <script>
-            const tagImage = document.querySelector('.img-preview');
-            const inputImage = document.querySelector('.input-file');
-
-            inputImage.addEventListener('change', function() {
-                tagImage.src = URL.createObjectURL(inputImage.files[0]);
-            });
-
-            // let productPrice = $('#selling_price').val();
-            // function handleTransaction() {
-            //     let quantity = $('#quantity').val();
-            //     let productsId = $('#products_id').val();
-            //     if (quantity != '') {
-            //         $.ajax({
-            //             type: 'get',
-            //             url: '/transaction/get_package/' + quantity + '/' + productsId,
-            //             success: function(package) {
-            //                 if (package.status == 'success') {
-            //                     if (package.data != null) {
-            //                         $('#selling_price').val(package.data.selling_price);
-            //                         $('.text-mention').html('Kamu mendapatkan potongan sebesar Rp. ' + package.data.selling_price + ' karena membeli diatas ' + package.data.quantity + ' kuantitas produk');
-            //                     } else {
-            //                         $('#selling_price').val(productPrice);
-            //                         $('.text-mention').html('');
-            //                     }
-            //                     $('#total').val(quantity * $('#selling_price').val());
-            //                 }
-            //             }
-            //         });
-            //     } else {
-            //         $('#selling_price').val(productPrice);
-            //         $('#total').val('');
-            //     }
-            // }
-
-            // $(document).ready(function() {
-            //     $('#total').val(parseInt($('#quantity').val()) * parseInt(productPrice));
-            //     handleTransaction();
-            // });
-
-            // $(document).on('change', '#quantity', function() {
-            //     handleTransaction();
-            // });
-        </script>
-    @endpush
 @endsection
