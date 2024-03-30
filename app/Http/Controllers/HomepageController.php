2e85cdd6f71ca108af4134af87cae54182185a03
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-30 18:38:22 +0800

    Progress: Homepage notification

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -32,46 +32,88 @@ class HomepageController extends Controller
 
     public function products(): View
     {
+        if (auth()->user() != null && auth()->user()->role == 'reseller') {
+            $transactions = Transaction::whereRaw('created_at <> updated_at')->where('status', 2)->get();
+        } else {
+            $transactions = [];
+        }
+
         return view('homepage.products', [
             'title' => 'Halaman Produk',
             'products' => $this->product->findAll(),
+            'transactions' => $transactions,
         ]);
     }
 
     public function product(int $id): View
     {
+        if (auth()->user() != null && auth()->user()->role == 'reseller') {
+            $transactions = Transaction::whereRaw('created_at <> updated_at')->where('status', 2)->get();
+        } else {
+            $transactions = [];
+        }
+
         return view('homepage.product', [
             'title' => 'Halaman Produk',
             'product' => $this->product->findById($id),
+            'transactions' => $transactions,
         ]);
     }
 
     public function testimonial(): View
     {
+        if (auth()->user() != null && auth()->user()->role == 'reseller') {
+            $transactions = Transaction::whereRaw('created_at <> updated_at')->where('status', 2)->get();
+        } else {
+            $transactions = [];
+        }
+
         return view('homepage.testimonial', [
             'title' => 'Halaman Kontak',
+            'transactions' => $transactions,
         ]);
     }
 
     public function contact(): View
     {
+        if (auth()->user() != null && auth()->user()->role == 'reseller') {
+            $transactions = Transaction::whereRaw('created_at <> updated_at')->where('status', 2)->get();
+        } else {
+            $transactions = [];
+        }
+
         return view('homepage.contact', [
             'title' => 'Halaman Kontak',
+            'transactions' => $transactions,
         ]);
     }
 
     public function profile(): View
     {
+        if (auth()->user() != null && auth()->user()->role == 'reseller') {
+            $transactions = Transaction::whereRaw('created_at <> updated_at')->where('status', 2)->get();
+        } else {
+            $transactions = [];
+        }
+
         return view('homepage.profile', [
             'title' => 'Halaman Profil',
             'profile' => $this->customer->findByUserId(auth()->user()->id),
+            'transactions' => $transactions,
         ]);
     }
 
     public function orderCompleted(): View
     {
+        if (auth()->user() != null && auth()->user()->role == 'reseller') {
+            $transactions = Transaction::whereRaw('created_at <> updated_at')->where('status', 2)->get();
+        } else {
+            $transactions = [];
+        }
+
         return view('homepage.order-completed', [
             'title' => 'Halaman Order Berhasil',
+            'transactions' => $transactions,
         ]);
     }
 }
