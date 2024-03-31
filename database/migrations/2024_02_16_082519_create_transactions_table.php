4c97c50ca8d2061dab223b56009cd910f46d2ec7
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-31 11:33:12 +0800

    Progress: Update transaction migration

--- database/migrations/2024_02_16_082519_create_transactions_table.php
+++ database/migrations/2024_02_16_082519_create_transactions_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->integer('quantity');
             $table->enum('shipping', ['ekspedisi', 'offline']);
             $table->integer('shipping_price')->nullable();
+            $table->string('shipping_address')->nullable();
             $table->integer('price_per_product')->nullable();
             $table->integer('total_per_product')->nullable();
             $table->integer('total')->nullable();
