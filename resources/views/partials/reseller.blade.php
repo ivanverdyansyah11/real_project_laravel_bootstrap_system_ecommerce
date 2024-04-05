5f4ee2df5e0a8f4e7bb889da8bd251e854096753
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-06 00:17:59 +0800

    Progress: Change karyawan to reseller index reseller page

--- resources/views/partials/reseller.blade.php
+++ resources/views/partials/reseller.blade.php
@@ -2,18 +2,18 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="deleteModalLabel">Modal Hapus Karyawan</h1>
+                <h1 class="modal-title fs-5" id="deleteModalLabel">Modal Hapus Reseller</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form id="buttonDeleteReseller" method="POST" class="d-inline-block w-100">
                 @csrf
                 @method("DELETE")
                 <div class="modal-body">
-                    <p>Apakah kamu yakin untuk menghapus karyawan ini?</p>
+                    <p>Apakah kamu yakin untuk menghapus reseller ini?</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Batal Hapus</button>
-                    <button type="submit" class="btn btn-primary">Hapus Karyawan</button>
+                    <button type="submit" class="btn btn-primary">Hapus Reseller</button>
                 </div>
             </form>
         </div>
@@ -24,18 +24,18 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="approveModalLabel">Modal Setujui Karyawan</h1>
+                <h1 class="modal-title fs-5" id="approveModalLabel">Modal Setujui Reseller</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form id="buttonApprovedReseller" method="POST" class="d-inline-block w-100">
                 @csrf
                 @method("PUT")
                 <div class="modal-body">
-                    <p>Apakah kamu yakin untuk menyetujui karyawan ini?</p>
+                    <p>Apakah kamu yakin untuk menyetujui reseller ini?</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Batal Setujui</button>
-                    <button type="submit" class="btn btn-primary">Setujui Karyawan</button>
+                    <button type="submit" class="btn btn-primary">Setujui Reseller</button>
                 </div>
             </form>
         </div>
