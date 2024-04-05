b0939f4b049b8b790a17937064b4ac3eedd16d68
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-05 22:08:30 +0800

    Progress: Update product image controller

--- app/Http/Controllers/ProductImageController.php
+++ app/Http/Controllers/ProductImageController.php
@@ -3,31 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProductImageRequest;
-use App\Models\ProductImage;
 use App\Repositories\ProductImageRepositories;
-use Illuminate\Http\Request;
+use App\Utils\UploadFile;
 
 class ProductImageController extends Controller
 {
     public function __construct(
         protected readonly ProductImageRepositories $productImage,
+        protected readonly UploadFile $uploadFile,
     ) {
     }
 
-    // public function store(StoreProductImageRequest $request)
-    // {
-    //     try {
-    //         dd($request->validated());
-    //         $this->productImage->store($request->validated());
-    //         return redirect()->back()->with('success', 'Berhasil ganti thumbnail gambar produk!');
-    //     } catch (\Exception $e) {
-    //         return redirect()->back()->with('error', 'Gagal ganti thumbnail gambar produk!');
-    //     }
-    // }
+    public function store(StoreProductImageRequest $request)
+    {
+        try {
+            // $request['image'] = $this->uploadFile->uploadSingleFile($request['image'], "assets/images/product");
+            // dd($request);
+            $this->productImage->store($request->validated());
+            return redirect()->back()->with('success', 'Berhasil ganti thumbnail gambar produk!');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Gagal ganti thumbnail gambar produk!');
+        }
+    }
 
     public function edit(int $id)
     {
-        dd('123');
         return view('product.product-image.index', [
             'title' => 'Halaman Edit Thumbnail Produk',
             'product_images' => $this->productImage->findAllWhereByproductId($id),
