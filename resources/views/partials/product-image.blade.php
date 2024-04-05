e9a96400ca27665fb503445e1d6a84443f376ed1
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-05 22:09:41 +0800

    Progress: Update product image partials modal

--- resources/views/partials/product-image.blade.php
+++ resources/views/partials/product-image.blade.php
@@ -1,3 +1,39 @@
+<div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="addModalLabel">Modal Tanbah Gambar Produk</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="{{ route('thumbnail-image.create') }}" method="POST" class="d-inline-block w-100"
+                enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <input type="hidden" name="products_id" value="{{ $product_images[0]->products_id }}">
+                    <input type="hidden" name="status" value="0">
+                    <div class="col mb-3 d-flex flex-column">
+                        <label for="image" class="form-label">Foto Produk</label>
+                        <img src="{{ asset('assets/images/other/img-not-found.jpg') }}" alt="Image Not Found"
+                            class="rounded mb-2 img-preview" width="100" height="100" style="object-fit: cover;">
+                        <input required type="file"
+                            class="form-control input-file @error('image') is-invalid @enderror" name="image"
+                            id="image">
+                        @error('image')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Batal Tambah</button>
+                    <button type="submit" class="btn btn-primary">Tambah Gambar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <div class="modal fade" id="thumbnailModal" tabindex="-1" aria-labelledby="thumbnailModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
