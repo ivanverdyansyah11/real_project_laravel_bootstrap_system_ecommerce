9355b27b4fc47d08eb3bd4036218c489fde6690f
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-05 22:09:22 +0800

    Progress: Update product homepage page

--- resources/views/homepage/product.blade.php
+++ resources/views/homepage/product.blade.php
@@ -5,7 +5,17 @@
         <div class="row justify-content-center">
             <div class="col-4">
                 <img src="{{ file_exists('assets/images/product/' . $product->image) && $product->image ? asset('assets/images/product/' . $product->image) : asset('assets/images/other/img-not-found.jpg') }}"
-                    class="card-img-top img-fluid rounded">
+                    class="card-img-top img-fluid rounded thumbnail-img" style="aspect-ratio: 1/1; object-fit: cover;">
+                <div class="scroll-image mt-3" style="overflow: auto; width: 100%;">
+                    <div class="wrapper d-flex gap-2" style="width: fit-content;">
+                        @foreach ($product_images as $product_image)
+                            <img src="{{ file_exists('assets/images/product/' . $product_image->image) ? asset('assets/images/product/' . $product_image->image) : asset('assets/images/other/img-not-found.jpg') }}"
+                                class="rounded product-img {{ $product->image == $product_image->image ? 'opacity-100' : 'opacity-50' }}"
+                                alt="Product Image" width="100" height="100"
+                                style="object-fit: cover; cursor: pointer;" onclick="changeThumbnailImage(this)">
+                        @endforeach
+                    </div>
+                </div>
             </div>
             <div class="col-6">
                 <div class="wrapper d-flex align-items-center justify-content-between mb-3">
@@ -39,4 +49,21 @@
             </div>
         </div>
     </div>
+
+    @push('js')
+        <script>
+            const productImg = document.querySelectorAll('.product-img')
+            const thumbnailImg = document.querySelector('.thumbnail-img')
+
+            function changeThumbnailImage(element) {
+                productImg.forEach(image => {
+                    image.classList.remove('opacity-100')
+                    image.classList.add('opacity-50')
+                });
+                thumbnailImg.setAttribute('src', element.getAttribute('src'))
+                element.classList.remove('opacity-50')
+                element.classList.add('opacity-100')
+            }
+        </script>
+    @endpush
 @endsection
