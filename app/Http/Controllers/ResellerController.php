a1c32ece108c1b7dc7c16067dbad89c1ec40cb20
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-16 06:10:11 +0800

    Progress: Update reseller controller

--- app/Http/Controllers/ResellerController.php
+++ app/Http/Controllers/ResellerController.php
@@ -6,29 +6,39 @@ use App\Http\Requests\StoreResellerRequest;
 use App\Http\Requests\UpdateResellerRequest;
 use App\Models\Reseller;
 use App\Repositories\ResellerRepositories;
+use App\Repositories\TransactionRepositories;
 use Illuminate\Contracts\View\View;
 
 class ResellerController extends Controller
 {
     public function __construct(
         protected readonly ResellerRepositories $reseller,
-    ) {}
+        protected readonly TransactionRepositories $transaction,
+    ) {
+    }
 
-    public function index() {
+    public function index()
+    {
         return view('reseller.index', [
             'title' => 'Halaman Reseller',
             'resellers' => $this->reseller->findAll(),
+            'transaction_pendings' => $this->transaction->findAllWherePending(),
+            'transaction_payments' => $this->transaction->findAllWherePayment(),
         ]);
     }
 
-    public function show(Reseller $reseller) : View {
+    public function show(Reseller $reseller): View
+    {
         return view('reseller.detail', [
             'title' => 'Halaman Detail Reseller',
             'reseller' => $this->reseller->findById($reseller->id),
+            'transaction_pendings' => $this->transaction->findAllWherePending(),
+            'transaction_payments' => $this->transaction->findAllWherePayment(),
         ]);
     }
 
-    public function approved(int $id) {
+    public function approved(int $id)
+    {
         try {
             $this->reseller->approved($id);
             return redirect()->route('reseller.index')->with('success', 'Berhasil menyetujui reseller');
@@ -37,13 +47,17 @@ class ResellerController extends Controller
         }
     }
 
-    public function create() : View {
+    public function create(): View
+    {
         return view('reseller.add', [
             'title' => 'Halaman Tambah Reseller',
+            'transaction_pendings' => $this->transaction->findAllWherePending(),
+            'transaction_payments' => $this->transaction->findAllWherePayment(),
         ]);
     }
 
-    public function store(StoreResellerRequest $request) {
+    public function store(StoreResellerRequest $request)
+    {
         try {
             $this->reseller->store($request->validated());
             return redirect()->route('reseller.index')->with('success', 'Berhasil membuat reseller baru');
@@ -52,14 +66,18 @@ class ResellerController extends Controller
         }
     }
 
-    public function edit(Reseller $reseller) : View {
+    public function edit(Reseller $reseller): View
+    {
         return view('reseller.edit', [
             'title' => 'Halaman Edit Reseller',
             'reseller' => $this->reseller->findById($reseller->id),
+            'transaction_pendings' => $this->transaction->findAllWherePending(),
+            'transaction_payments' => $this->transaction->findAllWherePayment(),
         ]);
     }
 
-    public function update(UpdateResellerRequest $request, Reseller $reseller) {
+    public function update(UpdateResellerRequest $request, Reseller $reseller)
+    {
         try {
             $this->reseller->update($request->validated(), $reseller);
             return redirect()->route('reseller.index')->with('success', 'Berhasil edit reseller');
@@ -68,7 +86,8 @@ class ResellerController extends Controller
         }
     }
 
-    public function destroy(Reseller $reseller) {
+    public function destroy(Reseller $reseller)
+    {
         try {
             $this->reseller->delete($reseller);
             return redirect(route('reseller.index'))->with('success', 'Berhasil hapus reseller!');
