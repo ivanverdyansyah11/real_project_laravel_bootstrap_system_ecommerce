b0d46b65abfbc1bc532bfda82d7f95d285c7e723
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-16 06:10:03 +0800

    Progress: Update profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use App\Repositories\AdminRepositories;
 use App\Repositories\CustomerRepositories;
 use App\Repositories\ProfileRepositories;
 use App\Repositories\ResellerRepositories;
+use App\Repositories\TransactionRepositories;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 
@@ -17,23 +18,31 @@ class ProfileController extends Controller
         protected readonly ResellerRepositories $reseller,
         protected readonly CustomerRepositories $customer,
         protected readonly ProfileRepositories $profile,
-    ) {}
+        protected readonly TransactionRepositories $transaction,
+    ) {
+    }
 
-    public function index() : View {
+    public function index(): View
+    {
         if (auth()->user()->role == 'super_admin' || auth()->user()->role == 'admin') {
             return view('profile.index', [
                 'title' => 'Halaman Profil',
                 'profile' => $this->admin->findById(auth()->user()->admin->id),
+                'transaction_pendings' => $this->transaction->findAllWherePending(),
+                'transaction_payments' => $this->transaction->findAllWherePayment(),
             ]);
         } else {
             return view('profile.index', [
                 'title' => 'Halaman Profil',
                 'profile' => $this->reseller->findByUserId(auth()->user()->id),
+                'transaction_pendings' => $this->transaction->findAllWherePending(),
+                'transaction_payments' => $this->transaction->findAllWherePayment(),
             ]);
         }
     }
 
-    public function update(UpdateProfileRequest $request) : RedirectResponse {
+    public function update(UpdateProfileRequest $request): RedirectResponse
+    {
         try {
             if ($request->password !== $request->confirmation_password) {
                 return redirect(route('profile.index'))->with('failed-password', "Password tidak sesuai!");
