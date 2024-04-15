693bc9b3e7a554b9ea2bc4a802bfe68d1de96f39
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-04-16 06:10:37 +0800

    Progress: Update transaction reward controller

--- app/Http/Controllers/TransactionRewardController.php
+++ app/Http/Controllers/TransactionRewardController.php
@@ -3,43 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTransactionRewardRequest;
-use App\Models\TransactionReward;
 use App\Repositories\ResellerRepositories;
 use App\Repositories\RewardRepositories;
+use App\Repositories\TransactionRepositories;
 use App\Repositories\TransactionRewardRepositories;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 class TransactionRewardController extends Controller
 {
     public function __construct(
         protected readonly TransactionRewardRepositories $transaction,
+        protected readonly TransactionRepositories $transactions,
         protected readonly ResellerRepositories $reseller,
         protected readonly RewardRepositories $reward,
-    ) {}
+    ) {
+    }
 
-    public function index(): View {
+    public function index(): View
+    {
         if (auth()->user()->role == 'super_admin' || auth()->user()->role == 'admin') {
             return view('report-reward.index', [
                 'title' => 'Halaman Rekap Poin',
                 'transactions' => $this->transaction->findAll(),
+                'transaction_pendings' => $this->transactions->findAllWherePending(),
+                'transaction_payments' => $this->transactions->findAllWherePayment(),
             ]);
-        } elseif(auth()->user()->role == 'reseller') {
+        } elseif (auth()->user()->role == 'reseller') {
             return view('report-reward.index', [
                 'title' => 'Halaman Rekap Poin',
                 'transactions' => $this->transaction->findAllByReseller(auth()->user()->id),
+                'transaction_pendings' => $this->transactions->findAllWherePending(),
+                'transaction_payments' => $this->transactions->findAllWherePayment(),
             ]);
         }
     }
 
-    public function show(int $transaction_id): View {
+    public function show(int $transaction_id): View
+    {
         return view('report-reward.detail', [
             'title' => 'Halaman Detail Rekap Poin',
             'transaction' => $this->transaction->findById($transaction_id),
         ]);
     }
 
-    public function store(StoreTransactionRewardRequest $request) : RedirectResponse {
+    public function store(StoreTransactionRewardRequest $request): RedirectResponse
+    {
         try {
             $reseller = $this->reseller->findByUserId($request['resellers_id']);
             $reward = $this->reward->findById($request['rewards_id']);
