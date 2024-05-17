dce6f5d633d1aafad3e2bd4bb619504392a75cb8
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-05-17 23:30:38 +0800

    Progress: Add new model management product

---
+++ app/Models/ManagementProduct.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ManagementProduct extends Model
+{
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    public function product() {
+        return $this->belongsTo(Product::class, 'products_id');
+    }
+}
