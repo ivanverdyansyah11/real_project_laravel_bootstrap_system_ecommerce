98fcb074a213c2221f0f6b9da0cbbeb585337e62
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-05-28 02:17:55 +0800

    Progress: update add with trashed transaction reward model

--- app/Models/TransactionReward.php
+++ app/Models/TransactionReward.php
@@ -11,10 +11,10 @@ class TransactionReward extends Model
     protected $guarded = ['id'];
 
     public function reward() {
-        return $this->belongsTo(Reward::class, 'rewards_id');
+        return $this->belongsTo(Reward::class, 'rewards_id')->withTrashed();
     }
 
     public function reseller() {
-        return $this->belongsTo(Reseller::class, 'resellers_id');
+        return $this->belongsTo(Reseller::class, 'resellers_id')->withTrashed();
     }
 }
