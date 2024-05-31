3dc7151d6c82bf54c567a52a10fb9173594da332
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-06-01 01:00:33 +0800

    Progress: update transaction export

--- app/Exports/TransactionsExport.php
+++ app/Exports/TransactionsExport.php
@@ -9,12 +9,12 @@ class TransactionsExport implements FromCollection
 {
     public function collection()
     {
-        $transactions = Transaction::with(['product'])->whereIn('status', [0, 1])->get();
+        $transactions = Transaction::with(['product', 'reseller', 'payment'])->whereIn('status', [0, 1])->get();
         $transactionArray = [];
-        $transactionArray[] = ['INVOIS', 'NAMA PRODUK', 'JUMLAH', 'SATUAN', 'TOTAL'];
+        $transactionArray[] = ['INVOIS', 'NAMA PRODUK', 'NAMA RESELLER', 'PEMBAYARAN BANK', 'NAMA PEMBELI', 'JUMLAH', 'PENGIRIMAN', 'ALAMAT', 'SATUAN', 'TOTAL BAYAR', 'TOTAL DIBAYAR', 'TANGGAL & WAKTU TRANSAKSI'];
         foreach ($transactions as $transaction) {
             $transactionPrice = $transaction->total_per_product != null ? $transaction->total_per_product : $transaction->total;
-            $transactionArray[] = [$transaction->invois, $transaction->product->name, $transaction->quantity, $transaction->price_per_product, $transactionPrice];
+            $transactionArray[] = [$transaction->invois, $transaction->product->name, $transaction->reseller->name, $transaction->payment->bank_name, $transaction->buyers_name != null ? $transaction->buyers_name : '-', $transaction->quantity, $transaction->shipping, $transaction->address != null ? $transaction->address : '-', 'Rp. ' . number_format($transaction->price_per_product, 2, ',', '.'), 'Rp. ' . number_format($transactionPrice, 2, ',', '.'), 'Rp. ' . number_format($transaction->total_payment, 2, ',', '.'), $transaction->created_at];
         }
         return collect($transactionArray);
     }
