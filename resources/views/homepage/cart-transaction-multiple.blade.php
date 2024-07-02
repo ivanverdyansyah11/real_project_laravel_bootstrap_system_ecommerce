a46b9b4ce9d4fe1544c24a69827f237ef6e48a4d
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-07-02 18:35:13 +0800

    Progress: Update cart transaction payment page

--- resources/views/homepage/cart-transaction-multiple.blade.php
+++ resources/views/homepage/cart-transaction-multiple.blade.php
@@ -267,6 +267,26 @@
                         $(`#total${index}`).val('');
                     }
                 });
+
+                $(`#selling_price${index}`).val(formatRupiah($(`#selling_price${index}`).val(), 'Rp. '))
+                $(`#total${index}`).val(formatRupiah($(`#total${index}`).val(), 'Rp. '))
+
+                function formatRupiah(angka, prefix)
+                {
+                    let number_string = angka.replace(/[^,\d]/g, '').toString(),
+                        split    = number_string.split(','),
+                        sisa     = split[0].length % 3,
+                        rupiah     = split[0].substr(0, sisa),
+                        ribuan     = split[0].substr(sisa).match(/\d{3}/gi);
+
+                    if (ribuan) {
+                        separator = sisa ? '.' : '';
+                        rupiah += separator + ribuan.join('.');
+                    }
+
+                    rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+                    return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+                }
             })
         </script>
     @endpush
